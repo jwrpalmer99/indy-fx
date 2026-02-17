d4b7558fe4c7903390c4eb2d28c99f863ffca29b
Jeremy Palmer <jeremyp@outlook.com>
2026-02-17 22:19:56 +0000

    handle rgbsmall texture remap

--- scripts/shaders/manager.js
+++ scripts/shaders/manager.js
@@ -86,6 +86,10 @@ const SHADERTOY_MEDIA_REPLACEMENTS = new Map([
     "https://www.shadertoy.com/media/a/08b42b43ae9d3c0605da11d0eac86618ea888e62cdd9518ee8b9097488b31560.png",
     "modules/indy-fx/images/white_rgb_noise.png",
   ],
+  [
+    "https://www.shadertoy.com/media/a/3083c722c0c738cad0f468383167a0d246f91af2bfa373e9c5c094fb8c8413e0.png",
+    "modules/indy-fx/images/rgbsmall.png",
+  ]
 ]);
 const THUMBNAIL_SIZE = 256;
 const THUMBNAIL_CAPTURE_SECONDS = 1.0;
